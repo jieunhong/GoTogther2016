6f5da4a98819efc9df5a4e7a3a06d3858c6693b8
HanNa Park <inhannani@e-mirim.hs.kr>
2016-09-13 16:04:49 +0900

    #2 사용자가 선택한 장소에 마커 표시하고 마커끼리 연결

--- app/src/main/java/emirim/hs/kr/gmap/MapsActivity.java
+++ app/src/main/java/emirim/hs/kr/gmap/MapsActivity.java
@@ -1,7 +1,7 @@
 package emirim.hs.kr.gmap;
 
-import android.*;
 import android.content.pm.PackageManager;
+import android.graphics.Color;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
@@ -12,11 +12,19 @@ import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.CircleOptions;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.PolylineOptions;
+import com.google.android.gms.maps.GoogleMap.OnMapClickListener;
+import com.google.android.gms.maps.GoogleMap.OnMapLongClickListener;
 
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+import java.util.ArrayList;
+
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback,OnMapClickListener,OnMapLongClickListener {
     private GoogleMap mMap;
+    PolylineOptions polylineOptions;
+    ArrayList<LatLng> arrayPoints;
 
     double latitude=37.554752;
     double longitude=126.970631;
@@ -33,38 +41,31 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mapFragment.getMapAsync(this);
     }
 
-    public void onMapLongClick(LatLng point){
-        mMap.clear();
-    }
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
         final LatLng Loc=new LatLng(latitude,longitude);
         mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(Loc,16));
-        // Add a marker in Sydney and move the camera
+
         if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION)//마시멜로우 이전방식에서 허가되어있으면 실행
                 == PackageManager.PERMISSION_GRANTED) {
             mMap.setMyLocationEnabled(true);
         } else {
             Toast.makeText(MapsActivity.this, "You have to accept to enjoy all app's services!", Toast.LENGTH_LONG).show();
             //허가요청하는 거
-                ActivityCompat.requestPermissions(this,
-                        new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION},
-                        MY_LOCATION_REQUEST_CODE);
+            ActivityCompat.requestPermissions(this,
+                    new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION},
+                    MY_LOCATION_REQUEST_CODE);
         }
+
+        arrayPoints = new ArrayList<LatLng>();
+
+        mMap.setOnMapClickListener(this);
+        mMap.setOnMapLongClickListener(this);
     }
 
-    //허가요청이 떨어졌을 때 안드로이드에 의해 호출되는 메서드 야옹2
+    //허가요청이 떨어졌을 때 안드로이드에 의해 호출되는 메서드
     @Override
     public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
         if (requestCode == MY_LOCATION_REQUEST_CODE) {
@@ -77,4 +78,35 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
             }
         }
     }
+
+    public void onMapClick(LatLng latLng){
+
+        //선택한 장소에 핀 꽂기
+        MarkerOptions marker = new MarkerOptions();
+        marker.position(latLng);
+        mMap.addMarker(marker);
+
+        polylineOptions = new PolylineOptions();
+        polylineOptions.color(Color.RED);
+        polylineOptions.width(5);
+        arrayPoints.add(latLng);
+        polylineOptions.addAll(arrayPoints);
+        mMap.addPolyline(polylineOptions);
+
+    }
+
+    public void onMapLongClick(LatLng point){
+        mMap.clear();
+        arrayPoints.clear();
+    }
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+
 }
